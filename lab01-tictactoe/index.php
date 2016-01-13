ce8d9f2803a880147532658ce66bdf707ead5a36
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-01-12 17:03:57 -0800

    Can now pass paramters through the URL

--- lab01-tictactoe/index.php
+++ lab01-tictactoe/index.php
@@ -1,15 +1,16 @@
 <?php
     echo 'Holla holla get dolla.';
     echo '<br/>';
-    $hours_worked = 8.5;
+    $hours_worked = $_GET['hours'];
     $rate = 22;
     $total = $hours_worked * $rate;
     echo 'I made like $'.$total,' today.';
     if ($total > 1000000) {
-        $wealth = 'poor';
-    } else {
         $wealth = 'rich';
+    } else {
+        $wealth = 'poor';
     }
     echo '<br/>';
     echo ($total > 0) ? 'I would say I\'m pretty '.$wealth.'.' :'Dag yo.';
 ?>
+
