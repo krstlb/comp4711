11aed380adcc73728d00ecc95b63d887d6f75207
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-01-12 16:58:47 -0800

    Added extremely complex arithmetic

--- lab01-tictactoe/index.php
+++ lab01-tictactoe/index.php
@@ -1,5 +1,15 @@
 <?php
-    $adj = 'awesome! ';
-    $num_probs = 0;
-    echo 'This lab is '.$adj,' I have run into '.$num_probs,' problems.';
+    echo 'Holla holla get dolla.';
+    echo '<br/>';
+    $hours_worked = 8.5;
+    $rate = 22;
+    $total = $hours_worked * $rate;
+    echo 'I made like $'.$total,' today.';
+    if ($total > 1000000) {
+        $wealth = 'poor';
+    } else {
+        $wealth = 'rich';
+    }
+    echo '<br/>';
+    echo ($total > 0) ? 'I would say I\'m pretty '.$wealth.'.' :'Dag yo.';
 ?>
