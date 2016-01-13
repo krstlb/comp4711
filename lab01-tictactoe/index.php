7c7d6c8c36ec248ee0ad41af5c32a9335ca7e9ac
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-01-12 17:15:14 -0800

    Simple tic tac toe using if else statements

--- lab01-tictactoe/index.php
+++ lab01-tictactoe/index.php
@@ -1,16 +1,31 @@
 <?php
-    echo 'Holla holla get dolla.';
-    echo '<br/>';
-    $hours_worked = $_GET['hours'];
-    $rate = 22;
-    $total = $hours_worked * $rate;
-    echo 'I made like $'.$total,' today.';
-    if ($total > 1000000) {
-        $wealth = 'rich';
-    } else {
-        $wealth = 'poor';
+    $position = $_GET['board'];
+    $squares = str_split($position);
+
+    if (!isset($_GET['board'])) echo 'No board set.';
+    else if (winner('x',$squares)) echo 'You win.';
+    else if (winner('o',$squares)) echo "HA I win.";
+    else echo 'No winner yet.';
+
+    function winner($token, $position) {
+        $won = false;
+        if (($position[0] == $token) &&
+            ($position[1] == $token) &&
+            ($position[2] == $token)) {
+            $won = true;
+        } else if (($position[3] == $token) &&
+            ($position[4] == $token) &&
+            ($position[5] == $token)) {
+            $won = true;
+        } else if (($position[6] == $token) &&
+            ($position[7] == $token) &&
+            ($position[8] == $token)) {
+            $won = true;
+        } else {
+            $won = false;
+        }
+        return $won;
     }
-    echo '<br/>';
-    echo ($total > 0) ? 'I would say I\'m pretty '.$wealth.'.' :'Dag yo.';
 ?>
+
 
